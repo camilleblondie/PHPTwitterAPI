2e5e0fede8e0f2870fb129bedcc6c467511a0bf3
Florian Magot <flo.magot@gmail.com>
2015-06-10 01:30:04 +0200

    Added GET /tweet/:id

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,32 +32,49 @@ $app->get('/docs', function() use ($app) {
     return view('docs');
 });
 
+// Handling sign in with twitter
 $app->get('/authorize', function() use ($app) {
-	session_start();
-	// callback from twitter
-	if (isset($_REQUEST['oauth_token']) && isset($_REQUEST['oauth_verifier'])) {
-		$request_token = [];
-		$request_token['oauth_token'] = $_SESSION['oauth_token'];
-		$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
-		if ($request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
-			// Abort! Something is wrong.
-		}
-		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
-		$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
-		// saving access token
-		$_SESSION['access_token'] = $access_token;
-		// testing
-		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-		$user = $connection->get("account/verify_credentials");
-		return response()->json($user);
-	} else {
-		// generating request token
-		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-		$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => url('/authorize')));
-		$_SESSION['oauth_token'] = $request_token['oauth_token'];
-		$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-		// building twitter authorize url
-		$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-	    return redirect($url);
-	}
+    session_start();
+    // callback from twitter
+    if (isset($_REQUEST['oauth_token']) && isset($_REQUEST['oauth_verifier'])) {
+        $request_token = [];
+        $request_token['oauth_token'] = $_SESSION['oauth_token'];
+        $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+        if ($request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
+            // Abort! Something is wrong.
+        }
+        $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+        $access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+        // saving access token
+        $_SESSION['access_token'] = $access_token;
+        // testing
+        $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+        $user = $connection->get("account/verify_credentials");
+        return response()->json($user);
+    } else {
+        // generating request token
+        $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+        $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => url('/authorize')));
+        $_SESSION['oauth_token'] = $request_token['oauth_token'];
+        $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+        // building twitter authorize url
+        $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+        return redirect($url);
+    }
+});
+
+// GET /tweet/:id
+$app->get('/tweet/{id}', function($id) {
+    session_start();
+    if (isset($_SESSION['access_token']))
+    {
+        $access_token = $_SESSION['access_token'];
+        $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+        $status = $connection->get("statuses/show", array("id" => $id));
+        if ($connection->getLastHttpCode() == 200)
+            return response()->json(['text' => $status->text]);
+        else
+            return response()->json(['error' => 'This tweet does not exist.']);     
+    }
+    return redirect(url('/'));
 });
