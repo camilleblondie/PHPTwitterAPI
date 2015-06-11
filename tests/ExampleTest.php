3fa2c24b321203f789604a5a601ef2aabfe773bd
Jean Reptile <jeanreptile@gmail.com>
2015-06-11 16:16:25 +0200

    Followers test added.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,4 +14,18 @@ class ExampleTest extends TestCase {
         $this->assertResponseOk();
     }
 
+    public function testGetFollowers()
+    {
+        $response = $this->call('GET', '/followings?screen_name=test_php_2015&count=1');
+        $respJson = json_decode($response->getContent());
+        $dataTest = array(
+            array(
+                'screen_name' => "jean_reptile"
+            )
+        );
+        $respTest = json_decode(json_encode($dataTest));
+
+        $this->assertEquals($respJson, $respTest);
+    }
+
 }
