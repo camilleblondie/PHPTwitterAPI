55f61dfc9a980c9ef0d440e7449cb6b640f1e8f4
Jean Reptile <jeanreptile@gmail.com>
2015-06-12 19:35:34 +0200

    Script to launch every tests.

---
+++ testing.sh
@@ -0,0 +1,4 @@
+./vendor/bin/phpunit tests/SuccessfulUnitTest.php
+./vendor/bin/phpunit tests/FollowersTest.php
+./vendor/bin/phpunit tests/FavoritesTest.php
+./vendor/bin/phpunit tests/ErrorUnitTest.php
